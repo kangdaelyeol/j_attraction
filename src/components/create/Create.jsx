5d9b5f151d2dae2be7f075f5397d2ce66d0916c5
daniel <asa47972000@naver.com>
2023-05-25 16:56:06 +0900

    create supplement

--- src/components/create/Create.jsx
+++ src/components/create/Create.jsx
@@ -1,19 +1,81 @@
-import React from "react";
-import Styles from "./create.module.css";
-
+import React, { useState } from 'react';
+import Styles from './create.module.css';
+import Day from './Day';
 /** required
  * google Map
  * title
  * summary(people, location, days, weather, budget, category)
- * route (locations of places)
- * place (spendingTime, price, des), moving(way, spendingtime, tips)
- * 
+ * Day[1, 2, 3]
+ * * route (locations of places)
+ * * place (spendingTime, price, des), moving(way, spendingtime, tips), pictures[]
  */
 
 const Create = () => {
-  return (
-    <div className={Styles.container}>Create</div>
-  )
-}
+	const [day, setDay] = useState([]);
+
+	const onAddDayClick = () => {
+		setDay((day) => {
+			const newday = { day: day.length + 1 };
+			return [...day, newday];
+		});
+	};
+
+	const onRemoveDayClick = (index) => {
+		let dayIndex = 1;
+		const newDays = [];
+		day.forEach((v) => {
+			console.log(v);
+			if (Number(v.day) !== Number(index)) {
+				v.day = dayIndex;
+				newDays.push({
+					...v,
+					day: dayIndex++,
+				});
+			}
+		});
+		console.log(index, newDays);
+		return setDay(newDays);
+	};
+
+	return (
+		<div className={Styles.container}>
+			<div className='title'>Create</div>
+			<input type='text' placeholder='Title' />
+			<div className='summary'>
+				<select name='season' id='season'>
+					<option value=''>계절</option>
+					<option value='spring'>봄</option>
+					<option value='summer'>여름</option>
+					<option value='autumn'>가을</option>
+					<option value='winter'>겨울</option>
+				</select>
+				<input type='text' placeholder='Budget' />
+				<input type='text' placeholder='Duration' />
+			</div>
+			<textarea name='introduction' id='introduction' cols='30' rows='10' placeholder='간단한 여행 소개'></textarea>
+			<div className='daybox'>
+				{day.map((v, index) => {
+					return (
+						<div
+							key={index}
+							data-index={v.day}
+							onClick={(e) => {
+								onRemoveDayClick(v.day);
+							}}
+							className='day'
+						>
+							day{v.day}
+						</div>
+					);
+				})}
+				<div onClick={onAddDayClick} className='addday'>
+					addDay
+				</div>
+			</div>
+			<Day />
+      <button className="save">save!</button>
+		</div>
+	);
+};
 
 export default Create;
---
+++ src/components/create/Day.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Styles from './day.module.css';
+
+const Day = () => {
+	return (
+		<div className='container'>
+			<div className='map'>
+				<span>make your route</span>
+				<div className='google__map'>googleMap</div>
+			</div>
+
+		</div>
+	);
+};
+
+export default Day;
