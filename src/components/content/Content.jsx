22b15382bcf7a6d36c22d0e529710a395e475b17
daniel <asa47972000@naver.com>
2023-06-19 00:48:33 +0900

    add fontawesome, individual expresstion

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,59 +1,89 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Styles from './content.module.css';
+import Detail from './Detail';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faHeart,
+	faComment,
+	faBookmark,
+} from '@fortawesome/free-regular-svg-icons';
 
-const Content = ({trip}) => {
-  const {budget, category, createdAt, date, picture} = trip;
-  const [boxPosition, setBoxPosition] = useState(0);
-  const LEFTUNIT = 500;
-  const MAXLEFT = (picture.length-1) * LEFTUNIT;
-  const onLeftClick = () => {
-    if(-MAXLEFT >= boxPosition) return;
-    boxRef.current.style.left = `${boxPosition - LEFTUNIT}px`;
-    setBoxPosition((c) => c - LEFTUNIT);
+const Content = ({ trip }) => {
+	const { title, category, createdAt, description, picture, owner } = trip;
+	console.log(trip);
+	const [boxPosition, setBoxPosition] = useState(0);
+  const [detail, setDetail] = useState(false);
+	const LEFTUNIT = 500;
+	const MAXLEFT = (picture.length - 1) * LEFTUNIT;
+	const onLeftClick = () => {
+		if (-MAXLEFT < boxPosition) return;
+		boxRef.current.style.left = `${boxPosition + LEFTUNIT}px`;
+		setBoxPosition((c) => c + LEFTUNIT);
+	};
+
+  const onMiddleClick = () => {
+    setDetail(true);
   }
 
-  const onRightClick = () => {
-    if(-MAXLEFT < boxPosition) return
-    boxRef.current.style.left = `${boxPosition + LEFTUNIT}px`;
-    setBoxPosition((c) => c + LEFTUNIT);
-  }
-  const boxRef = useRef();
+	const onRightClick = () => {
+		if (-MAXLEFT >= boxPosition) return;
+		boxRef.current.style.left = `${boxPosition - LEFTUNIT}px`;
+		setBoxPosition((c) => c - LEFTUNIT);
+	};
+	const boxRef = useRef();
+	useEffect(() => {
+		boxRef.current.style.left = '0px';
+	}, []);
 	return (
 		<div className={`${Styles.container}`}>
 			<div className={Styles.header}>
 				<div className={Styles.header__left}>
 					<div className={Styles.profile__picture}>
-            <img src="profile.png" alt="profile" />
-          </div>
+						<img
+							src='https://res.cloudinary.com/dfvqmpyji/image/upload/v1684905405/uploads/jzilk9x2ydftrtdinzst.jpg'
+							alt='profile'
+						/>
+					</div>
 					<div className={Styles.profile__info}>
 						<div className={Styles.info__top}>
-							<span className={Styles.owner}> ownerName </span> <div className={Styles.createdAt}>3일</div>
+							<span className={Styles.owner}>{owner.displayName}</span>{' '}
+							<div className={Styles.createdAt}>{createdAt.relative}</div>
 						</div>
-						<div className={Styles.info__bottom}>지역</div>
 					</div>
 				</div>
 				<div className={Styles.header__right}> ... </div>
 			</div>
-      <div className={Styles.body}>
-        <div ref={boxRef} className={Styles.picture__box}>
-          {picture.map((url, ind) => {
-            return <img src={url} key={ind} alt="thumb"/>
-          })}
-        </div>
-        <div className={Styles.left} onClick={onLeftClick}> </div>
-        <div className={Styles.right} onClick={onRightClick}> </div>
-      </div>
-      <div className={Styles.footer}>
-        <div className={Styles.footer__top}>
-          <div className={Styles.footer_t_left}>
-            likeBtn, commentBtn, sendBtn
-          </div>
-          <div className={Styles.footer_t_right}>SaveBtn</div>
-        </div>
-        <div className={Styles.footer__middle}>
-          ownerId description
-        </div>
-      </div>
+			<div className={Styles.body}>
+				<div ref={boxRef} className={Styles.picture__box}>
+					{picture.map((url, ind) => {
+						return <img src={url} key={ind} alt='thumb' />;
+					})}
+				</div>
+				<div className={Styles.left} onClick={onLeftClick}>
+					{' '}
+				</div>
+				<div className={Styles.middle} onClick={onMiddleClick}>
+					{' '}
+				</div>
+				<div className={Styles.right} onClick={onRightClick}>
+					{' '}
+				</div>
+			</div>
+			<div className={Styles.footer}>
+				<div className={Styles.footer__top}>
+					<div className={Styles.footer_t_left}>
+						<FontAwesomeIcon icon={faHeart} />{' '}
+						<FontAwesomeIcon icon={faComment} />
+					</div>
+					<div className={Styles.footer_t_right}>
+						<FontAwesomeIcon icon={faBookmark} />
+					</div>
+				</div>
+				<div className={Styles.footer__middle}>
+					{title} {description}
+				</div>
+			</div>
+      {detail ? <Detail trip={trip} setDetail={setDetail}/> : ""}
 		</div>
 	);
 };
---
+++ src/components/content/Detail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Styles from "./detail.module.css";
+
+const Detail = () => {
+
+  return (
+    <div className="container"></div>
+  )
+}
+
+export default Detail
